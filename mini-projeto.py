398b00328789d62f2f4be9189099980f902b972f
Flyxs <felypecarrijo19@gmail.com>
2024-11-28 12:36:19 -0300

    possivel ultima att

--- mini-projeto.py
+++ mini-projeto.py
@@ -1,6 +1,6 @@
 import os
 import time
-from teste_de_codigo import fmt_nome
+from teste_de_codigo import fmt_nome, testa_mat
 
 os.system('clear')
 
@@ -8,37 +8,41 @@ class Banco_de_notas:
     def __init__(self,nome,matricula,notas=None):
         self.nome = nome
         self.matricula = matricula
-        self.notas = notas if notas is not None else []
+        self.notas = notas if notas is not None else [0,0,0]
 
-    def Adcionar_nota(self,nota):
-        if len(self.notas) < 3:
-            self.notas.append(nota)
-            print('Nota adcionada com sucesso!')
-        else:
-            print('Banco de notas cheio')
+    def Adcionar_nota(self,pos,nota):
+        self.notas[pos-1] = int(nota)
+        print('Nota adcionada com sucesso!')
             
+    def Remove_nota(self,pos):
+        del self.notas[pos-1]
+        print(f'Nota {pos} removida com sucesso')
+        return self.notas
+    
+    def Calc_med_a(self):
+        media = sum(self.notas)/3
+        return media     
+             
     def Exibir_nota(self):
-        print(f'{self.nome}  {self.matricula}: {self.notas}')
+        print(f'{self.nome}  {self.matricula}: {self.notas} ')
         
     def __str__(self):
         return f'{self.nome}   {self.matricula}:  {self.notas}'  
     
-    def Calc_med_a(self):
-        media = sum(self.notas)/3
-        print()
         
 estudantes = []
 
 while True:
-    escolha = str(input(f'O que gostaria de fazer? \n [1]Adicionar um estudante \n [2]Consultar Nota Pessal \n [3]Exibir Resultado da turma \n [4]Adcionar Notas \n [q]Sair\n'))
+    escolha = str(input(f'O que gostaria de fazer? \n [1]Adicionar um estudante \n [2]Consultar Nota Pessoal \n [3]Exibir Resultado da turma \n [4]Adcionar Notas \n [5]Remover uma nota \n [q]Sair\n'))
     os.system('clear')
     
     if escolha == '1':
         nome = str(input('Qual o nome do estudante? '))
-        fmt_nome(nome)
+        nome = fmt_nome(nome)
         matricula = str(input('Qual a matricula? '))
-        estudantes.append(Banco_de_notas(nome,matricula))
-        print(f'O estudante {nome} foi adcionado com sucesso! \n')
+        if testa_mat(matricula):
+            estudantes.append(Banco_de_notas(nome,matricula))
+            print(f'O estudante {nome} foi adcionado com sucesso! \n')
         
         time.sleep(2)
         os.system('clear')
@@ -68,19 +72,32 @@ while True:
         for estudante in estudantes:
             if estudante.matricula == matricula:
                 encontrado = True
-                if len(estudante.notas) < 3:
-                    nota = float(input('Digite a nota: '))
-                    estudante.Adcionar_nota(nota)
+                pos = int(input('Qual a nota que gostária de modificar? [1] [2] [3]\n'))
+                nota = float(input('Digite a nota: ')) 
+                estudante.Adcionar_nota(pos,nota)
         if not encontrado:
             print('Estudante não encontrado')
             
         time.sleep(2)
         os.system('clear')
         
+    elif escolha == '5':
+        matricula = input('Qual a matrícula do aluno?\n')
+        encontrado = False
+        for estudante in estudantes:
+            if estudante.matricula == matricula:
+                encontrado = True
+                nota = int(input('Qual a prova que deseja remover? [1] [2] [3]'))
+                estudante.Remove_nota(nota)
+            if not encontrado:
+                print('Estudante não encontrado')
+
+        time.sleep(2)
+        os.system('clear')
         
     elif escolha == 'q':
         print('Obrigado por usar o nosso sistema! ')
         time.sleep(2)
         os.system('clear')
         break
-        
+    
--- teste_de_codigo.py
+++ teste_de_codigo.py
@@ -1,6 +1,12 @@
 def fmt_nome(a):
     nome = a.split(' ')
     nome = [i.capitalize() for i in nome]
-    print(' '.join(nome))
-    
-fmt_nome('jose alcantra soares')
+    nome = ' '.join(nome)
+    return nome
+
+def testa_mat(a):
+    if len(a) == 9:
+        return True
+    else:
+        print('Por favor insira uma matricula válida')
+        return False
